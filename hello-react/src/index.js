8e4a24d989ae11bd3f658403d9be04933e7c5bd0
broooooooo <bro@sktai.io>
2020-04-01 15:03:27 +0900

    Documented index.js

--- hello-react/src/index.js
+++ hello-react/src/index.js
@@ -1,12 +1,18 @@
+/*
+프로젝트를 번들링해주는 코드
+이런 모듈을 묶어서(bundle) 해주는 bundler가 웹팩, Parcel, browserify
+리액트에서는 웹팩 사용하는 추새
+*/
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
+ReactDOM.render( 
   <React.StrictMode>
-    <App />
+    <App />           {/*랜더링할 모듈*/}
   </React.StrictMode>,
   document.getElementById('root')
 );
