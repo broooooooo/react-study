2d1056d07e97214d83617e010e04353d3882473e
Nayoon Hwang <nayoon.hwang@sk.com>
2020-04-07 17:38:54 +0900

    claire chap1,2

---
+++ chapter1_2/claire/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './vue.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://vuejs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Vue
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
