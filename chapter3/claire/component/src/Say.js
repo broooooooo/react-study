e08c1f2e3795531716103bf3c82fd75a50225f55
Nayoon Hwang <nayoon.hwang@sk.com>
2020-04-16 17:58:54 +0900

    claire chap4

--- chapter3/claire/component/src/Say.js
+++ chapter3/claire/component/src/Say.js
@@ -1,7 +1,7 @@
-import React, {useState } from "react";
+import React, { useState } from "react";
 
 const Say = () => {
-    const [message, setMessage ] = useState('');
+    const [ message, setMessage ] = useState('');
     const onClickEnter = () => setMessage('안녕하세요!');
     const onClickLeave = () => setMessage('안녕히 가세요!');
 
@@ -14,7 +14,7 @@ const Say = () => {
             <h1 style={{color}}>{message}</h1>
             <button style={{color: 'red'}} onClick={() => setColor('red')}>빨간색</button>
             <button style={{color: 'green'}} onClick={() => setColor('green')}>초록색</button>
-            <button style={{ color: 'blue'}} onClick={() => setColor('blue')}>파란색</button>
+            <button style={{color: 'blue'}} onClick={() => setColor('blue')}>파란색</button>
         </div>
     );
 };
---
+++ chapter4/claire/event-practice/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import EventPractice from "./EventPractice";
+
+const App = () => {
+  return <EventPractice />;
+};
+
+export default App;
