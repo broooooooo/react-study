4755b0f15e2da11097b8ca170a8ba6ed4b077d4b
Juyeong Lee <ljy3795@gmail.com>
2020-05-31 15:18:38 +0900

    commit before yarn eject

---
+++ chapter9/colin/styling-react/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SassComponent from './SassComponent';
+
+function App() {
+  // return (
+  //   <div className="App">
+  //     {/* <header className="App-header"> */}
+  //     <header>
+  //       <img src={logo} className="logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  return (
+    <div>
+      <SassComponent/>
+    </div>
+  )
+}
+
+export default App;
